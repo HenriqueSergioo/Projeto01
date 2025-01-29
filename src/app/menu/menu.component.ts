50e93f7d2d7cc24dcdd9b7ea210238a768b2d035
HenriqueSergioo <henriquebrsergio@gmail.com>
2025-01-29 11:03:47 -0300

    Nav, Pagina de Hardware e Carrrinho feitos.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,12 +9,20 @@ import { Component } from '@angular/core';
 
 export class MenuComponent {
   produtos = [
-    { nome: 'Placa de Vídeo GTX 3060', preco: 2899.99, imagem: '../../assets/placa-de-video.jpg' },
-    { nome: 'Processador Intel i7', preco: 1899.99, imagem: '../../assets/processador.jpg' },
-    { nome: 'Memória RAM 16GB', preco: 699.99, imagem: '../../assets/ram.jpg' },
-    { nome: 'SSD 512GB', preco: 399.99, imagem: '../../assets/ssd.jpg' },
-    { nome: 'Fonte 750W', preco: 499.99, imagem: '../../assets/fonte.jpg' },
-    { nome: 'Placa Mãe ASUS', preco: 899.99, imagem: '../../assets/placa-mae.jpg' }
+    { nome: 'Placa de Vídeo GTX 4060', preco: 1986.90, imagem: '../../assets/Placa4060.jpg' },
+    { nome: 'Gabinete Preto ', preco: 374.99, imagem: '../../assets/gabinetepreto.jpg' },
+    { nome: 'Placa de Vídeo RTX 3060 ', preco: 1875.80, imagem: '../../assets/rtx3060.jpg' },
+    { nome: 'Memória RAM 16GB', preco: 287.90, imagem: '../../assets/memoriaram16.jpg' },
+    { nome: 'HD 500GB', preco: 219.79, imagem: '../../assets/hd500.jpg' },
+    { nome: 'Fonte 750W', preco: 499.99, imagem: '../../assets/fonte2.jpg' },
+    { nome: 'Placa Mãe ASUS', preco: 879.90, imagem: '../../assets/placamae1.jpg'},
+    { nome: 'Gabinete Branco ', preco: 410.45, imagem: '../../assets/gabinetebranco.jpg' },
+    { nome: 'Memória RAM 8GB', preco: 129.90, imagem: '../../assets/memoria8.jpg' },
+    { nome: 'SSD 500GB', preco: 325.99, imagem: '../../assets/ssdkingston.jpg' },
+    { nome: 'Fonte 500W', preco: 309.59, imagem: '../../assets/fonte1.jpg' },
+    { nome: 'Placa Mãe Z890', preco: 899.99, imagem: '../../assets/placamae2.jpg'},
+    { nome: 'Cooler Branco', preco: 690.70, imagem: '../../assets/coolerbranco.jpg' },
+    { nome: 'Cooler Preto', preco: 758.90, imagem: '../../assets/coolerpreto.jpg'}
   ];
 
   carrinho: { nome: string; preco: number; quantidade: number }[] = [];
--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
+  isNavHidden = false; // Estado da barra lateral
 
+  toggleNav() {
+    this.isNavHidden = !this.isNavHidden; // Alterna entre mostrar/esconder
+  }
 }
